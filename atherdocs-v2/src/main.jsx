b42878e34999e0d088ff4b558734d676def6cce4
Samuel Mazzilli <samuel.mazzilli13@gmail.com>
2026-02-19 19:47:27 +0100

    ccc

--- atherdocs-v2/src/main.jsx
+++ atherdocs-v2/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css' // 👈 Questa è la magia che trasforma il brutto anatroccolo!
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
