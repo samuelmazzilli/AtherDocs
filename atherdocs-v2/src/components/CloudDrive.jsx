cf449be78841b1ccc8f1c8f41979a11b9a30c704
Samuel Mazzilli <samuel.mazzilli13@gmail.com>
2026-02-20 08:43:20 +0100

    Renamed

--- atherdocs-v2/src/components/CloudDrive.jsx
+++ atherdocs-v2/src/components/CloudDrive.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { supabase } from '../supabaseClient';
+import { supabase } from '../supbaseCLient.js';
 import { Cloud, Lock, Mail, Key, LogOut, FileText, Plus, Server } from 'lucide-react';
 
 const CloudDrive = () => {
